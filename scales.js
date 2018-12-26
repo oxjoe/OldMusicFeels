0479d1042be3fa6de4b82beaa7db7618cb80ea75
Joseph Pong <oxjoe2006@yahoo.com>
2018-12-26 17:39:15 -0500

    need to look at express now

--- scales.js
+++ scales.js
@@ -1,84 +1,100 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
 const c_major = {
-  "key-name": "c_major",
-  "has-sharps": null,
-  "key-signature": 0,
-  "relative-minor": "Am"
+  'key-name': 'c_major',
+  'has-sharps': null,
+  'key-signature': 0,
+  'relative-minor': 'Am'
 }
 
 // KEYS WITH SHARPS
 const g_major = {
-  "key-name": "g_major",
-  "has-sharps": true,
-  "key-signature": 1,
-  "relative-minor": "Em"
+  'key-name': 'g_major',
+  'has-sharps': true,
+  'key-signature': 1,
+  'relative-minor': 'Em'
 }
 const d_major = {
-  "key-name": "d_major",
-  "has-sharps": true,
-  "key-signature": 2,
-  "relative-minor": "Bm"
+  'key-name': 'd_major',
+  'has-sharps': true,
+  'key-signature': 2,
+  'relative-minor': 'Bm'
 }
 const a_major = {
-  "key-name": "a_major",
-  "has-sharps": true,
-  "key-signature": 3,
-  "relative-minor": "F#m"
+  'key-name': 'a_major',
+  'has-sharps': true,
+  'key-signature': 3,
+  'relative-minor': 'F#m'
 }
 const e_major = {
-  "key-name": "e_major",
-  "has-sharps": true,
-  "key-signature": 4,
-  "relative-minor": "C#m"
+  'key-name': 'e_major',
+  'has-sharps': true,
+  'key-signature': 4,
+  'relative-minor': 'C#m'
 }
 const b_major = {
-  "key-name": "b_major",
-  "has-sharps": true,
-  "key-signature": 5,
-  "relative-minor": "G#m"
+  'key-name': 'b_major',
+  'has-sharps': true,
+  'key-signature': 5,
+  'relative-minor': 'G#m'
 }
 const f_sharp_major = {
-  "key-name": "f_sharp_major",
-  "has-sharps": true,
-  "key-signature": 6,
-  "relative-minor": "D#m",
-  "equivalent": "Gb"
+  'key-name': 'f_sharp_major',
+  'has-sharps': true,
+  'key-signature': 6,
+  'relative-minor': 'D#m',
+  'equivalent': 'Gb'
+}
+const c_sharp_major = {
+  'key-name': 'c_sharp_major',
+  'has-sharps': true,
+  'key-signature': 7,
+  'relative-minor': 'A#m',
+  'equivalent': 'Db'
 }
 
 // KEYS WITH FLATS
-const gb_major = {
-  "key-name": "gb_major",
-  "has-sharps": false,
-  "key-signature": 6,
-  "relative-minor": "Ebm",
-  "equivalent": "F#"
+const c_flat_major = {
+  'key-name': 'c_flat_major',
+  'has-sharps': false,
+  'key-signature': 7,
+  'relative-minor': 'Abm',
+  'equivalent': 'B'
 }
-const db_major = {
-  "key-name": "db_major",
-  "has-sharps": false,
-  "key-signature": 5,
-  "relative-minor": "Bbm"
+const g_flat_major = {
+  'key-name': 'g_flat_major',
+  'has-sharps': false,
+  'key-signature': 6,
+  'relative-minor': 'Ebm',
+  'equivalent': 'F#'
 }
-const ab_major = {
-  "key-name": "ab_major",
-  "has-sharps": false,
-  "key-signature": 4,
-  "relative-minor": "Fm"
+const d_flat_major = {
+  'key-name': 'd_flat_major',
+  'has-sharps': false,
+  'key-signature': 5,
+  'relative-minor': 'Bbm'
 }
-const eb_major = {
-  "key-name": "eb_major",
-  "has-sharps": false,
-  "key-signature": 3,
-  "relative-minor": "Cm"
+const a_flat_major = {
+  'key-name': 'a_flat_major',
+  'has-sharps': false,
+  'key-signature': 4,
+  'relative-minor': 'Fm'
 }
-const bb_major = {
-  "key-name": "bb_major",
-  "has-sharps": false,
-  "key-signature": 2,
-  "relative-minor": "Gm"
+const e_flat_major = {
+  'key-name': 'e_flat_major',
+  'has-sharps': false,
+  'key-signature': 3,
+  'relative-minor': 'Cm'
+}
+const b_flat_major = {
+  'key-name': 'b_flat_major',
+  'has-sharps': false,
+  'key-signature': 2,
+  'relative-minor': 'Gm'
 }
 const f_major = {
-  "key-name": "f_major",
-  "has-sharps": false,
-  "key-signature": 1,
-  "relative-minor": "Dm"
+  'key-name': 'f_major',
+  'has-sharps': false,
+  'key-signature': 1,
+  'relative-minor': 'Dm'
 }
--- script.js
+++ script.js
@@ -1,40 +1,77 @@
+/* eslint-disable no-undef */
 // https://github.com/jasonsjones/doubly-linked-list
 
 // takeaway IGNORE REQUIRE STUFF, JUST DO JAVASCRIPT STUFF (TESTS) AND VIDEOS, WHEN ADDING BACKEND IT SHOULD WORK
-//todo add a testing library
-/*NOTES: 1) Put dummy data into array
+// todo add a testing library
+/* NOTES: 1) Put dummy data into array
  *         so I can code unit test into it
  *         2) but array data into linkedlist
  *           code test to that
  *         3) Watch jest unit testing
  *         4) Watch express video then Mongo then from scratch */
 
-// TODO:FIRST imagine what it would be like if i scrapped through list of chords
 // Gets both "rn1" and "C"
 // console.log(document.getElementById("chord-0").innerText)
 
 // Gets only "C"
-//console.log(document.getElementById("chord-0").querySelector(".name").innerHTML)]
-
-//beefy script.js --live
+// console.log(document.getElementById("chord-0").querySelector(".name").innerHTML)]
 
 // let LinkedList = require('dbly-linked-list');
 // const list = new LinkedList()
 // console.log("list.isEmpty(): " + list.isEmpty())
 
-const arr = ["C", "Em", "Fadd9"]
+// 1) Scrape through and get chord names
+const noteNameArray = ['A', 'C#', 'Dadd9']
+document.getElementById('chord-0').querySelector('.name').innerHTML = noteNameArray[0]
+document.getElementById('chord-1').querySelector('.name').innerHTML = noteNameArray[1]
+document.getElementById('chord-2').querySelector('.name').innerHTML = noteNameArray[2]
 
-document.getElementById("chord-0").querySelector(".name").innerHTML = arr[0]
-document.getElementById("chord-1").querySelector(".name").innerHTML = arr[1]
-document.getElementById("chord-2").querySelector(".name").innerHTML = arr[2]
+// 2) Current key would be first chord
+document.getElementById('current-key').value = a_major['key-name']
 
-// --> true
+// 3) Add the roman numeral
+const noteQualityArray = [rn_ONE, rn_three, rn_four]
 
-const map = new Map()
+document.getElementById('chord-0').querySelector('.roman-num').innerHTML = noteQualityArray[0]
+document.getElementById('chord-1').querySelector('.roman-num').innerHTML = noteQualityArray[1]
+document.getElementById('chord-2').querySelector('.roman-num').innerHTML = noteQualityArray[2]
 
-const sharpsArray = ["F#", "C#", "G#", "D#", "A#", "E#", "B#"]
-const flatsArray = ["Bb", "Eb", "Ab", "Db", "Gb", "Cb", "Fb"]
-console.log(b_major["key-name"])
+const orderOfSharpsArray = ['F#', 'C#', 'G#', 'D#', 'A#', 'E#', 'B#']
+const orderOfFlatsArray = ['Bb', 'Eb', 'Ab', 'Db', 'Gb', 'Cb', 'Fb']
+console.log(a_major);
+
+// Definition of whole and half step
+const w = 2
+const h = 1
+
+let KEY = 'A'
+// For right now, I'll duplicate and figure out mod later
+const keyOfSharps = ['C', 'C#', 'D', 'D#', 'E', 'E#', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'B#', 'C', 'C#', 'D', 'D#', 'E', 'E#', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'B#']
+
+let startIndex = keyOfSharps.findIndex(function (element) {
+  return element === KEY
+})
+console.log(startIndex)
+
+let currentLocation = startIndex
+
+// TODO: Use jest to test!
+console.log("A major test start:");
+console.log(keyOfSharps[currentLocation])
+console.log(keyOfSharps[currentLocation += w])
+console.log(keyOfSharps[currentLocation += w])
+console.log(keyOfSharps[currentLocation += h])
+console.log(keyOfSharps[currentLocation += w])
+console.log(keyOfSharps[currentLocation += w])
+console.log(keyOfSharps[currentLocation += w])
+console.log(keyOfSharps[currentLocation += h])
+
+
+
+
+// console.log(b_major['key-name'])
+
+// PLAN: now i need to iterate through each .roman-num and add the quality of major or minor
 
 // C corresponds to 0, B corresponds to 11 and next C is 12
 // let noteNamesAndValues = [
