35a998a3f9a7af270ff0677efad35429c99c39df
Joseph Pong <oxjoe2006@yahoo.com>
2018-12-26 02:06:59 -0500

    test from vscode

---
+++ scales.js
@@ -0,0 +1,84 @@
+const c_major = {
+  "key-name": "c_major",
+  "has-sharps": null,
+  "key-signature": 0,
+  "relative-minor": "Am"
+}
+
+// KEYS WITH SHARPS
+const g_major = {
+  "key-name": "g_major",
+  "has-sharps": true,
+  "key-signature": 1,
+  "relative-minor": "Em"
+}
+const d_major = {
+  "key-name": "d_major",
+  "has-sharps": true,
+  "key-signature": 2,
+  "relative-minor": "Bm"
+}
+const a_major = {
+  "key-name": "a_major",
+  "has-sharps": true,
+  "key-signature": 3,
+  "relative-minor": "F#m"
+}
+const e_major = {
+  "key-name": "e_major",
+  "has-sharps": true,
+  "key-signature": 4,
+  "relative-minor": "C#m"
+}
+const b_major = {
+  "key-name": "b_major",
+  "has-sharps": true,
+  "key-signature": 5,
+  "relative-minor": "G#m"
+}
+const f_sharp_major = {
+  "key-name": "f_sharp_major",
+  "has-sharps": true,
+  "key-signature": 6,
+  "relative-minor": "D#m",
+  "equivalent": "Gb"
+}
+
+// KEYS WITH FLATS
+const gb_major = {
+  "key-name": "gb_major",
+  "has-sharps": false,
+  "key-signature": 6,
+  "relative-minor": "Ebm",
+  "equivalent": "F#"
+}
+const db_major = {
+  "key-name": "db_major",
+  "has-sharps": false,
+  "key-signature": 5,
+  "relative-minor": "Bbm"
+}
+const ab_major = {
+  "key-name": "ab_major",
+  "has-sharps": false,
+  "key-signature": 4,
+  "relative-minor": "Fm"
+}
+const eb_major = {
+  "key-name": "eb_major",
+  "has-sharps": false,
+  "key-signature": 3,
+  "relative-minor": "Cm"
+}
+const bb_major = {
+  "key-name": "bb_major",
+  "has-sharps": false,
+  "key-signature": 2,
+  "relative-minor": "Gm"
+}
+const f_major = {
+  "key-name": "f_major",
+  "has-sharps": false,
+  "key-signature": 1,
+  "relative-minor": "Dm"
+}
--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 // takeaway IGNORE REQUIRE STUFF, JUST DO JAVASCRIPT STUFF (TESTS) AND VIDEOS, WHEN ADDING BACKEND IT SHOULD WORK
 //todo add a testing library
-/*notes: 1) Put dummy data into array
-*         so I can code unit test into it
-*         2) but array data into linkedlist
-*           code test to that
-*         3) Watch jest unit testing
-*         4) Watch express video then Mongo then from scratch */
+/*NOTES: 1) Put dummy data into array
+ *         so I can code unit test into it
+ *         2) but array data into linkedlist
+ *           code test to that
+ *         3) Watch jest unit testing
+ *         4) Watch express video then Mongo then from scratch */
 
-// todo FIRST imagine what it would be like if i scrapped through list of chords
+// TODO:FIRST imagine what it would be like if i scrapped through list of chords
 // Gets both "rn1" and "C"
 // console.log(document.getElementById("chord-0").innerText)
 
@@ -18,9 +18,9 @@
 
 //beefy script.js --live
 
-let LinkedList = require('dbly-linked-list');
-const list = new LinkedList()
-console.log("list.isEmpty(): " + list.isEmpty())
+// let LinkedList = require('dbly-linked-list');
+// const list = new LinkedList()
+// console.log("list.isEmpty(): " + list.isEmpty())
 
 const arr = ["C", "Em", "Fadd9"]
 
@@ -30,6 +30,11 @@ document.getElementById("chord-2").querySelector(".name").innerHTML = arr[2]
 
 // --> true
 
+const map = new Map()
+
+const sharpsArray = ["F#", "C#", "G#", "D#", "A#", "E#", "B#"]
+const flatsArray = ["Bb", "Eb", "Ab", "Db", "Gb", "Cb", "Fb"]
+console.log(b_major["key-name"])
 
 // C corresponds to 0, B corresponds to 11 and next C is 12
 // let noteNamesAndValues = [
