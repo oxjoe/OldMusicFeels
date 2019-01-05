f71bd35ffc88bf71176f7a818503070c69e8a054
Joseph Pong <oxjoe2006@yahoo.com>
2019-01-05 01:16:24 -0500

    temporary fix for webpack

--- app.js
+++ app.js
@@ -1,4 +1,134 @@
 import style from "./views/index.css";
+
+//#region of scales.js
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+const c_major = {
+  'key-name': 'c_major',
+  'has-sharps': null,
+  'key-signature': 0,
+  'relative-minor': 'Am'
+}
+
+// KEYS WITH SHARPS
+const g_major = {
+  'key-name': 'g_major',
+  'has-sharps': true,
+  'key-signature': 1,
+  'relative-minor': 'Em'
+}
+const d_major = {
+  'key-name': 'd_major',
+  'has-sharps': true,
+  'key-signature': 2,
+  'relative-minor': 'Bm'
+}
+const a_major = {
+  'key-name': 'a_major',
+  'has-sharps': true,
+  'key-signature': 3,
+  'relative-minor': 'F#m'
+}
+const e_major = {
+  'key-name': 'e_major',
+  'has-sharps': true,
+  'key-signature': 4,
+  'relative-minor': 'C#m'
+}
+const b_major = {
+  'key-name': 'b_major',
+  'has-sharps': true,
+  'key-signature': 5,
+  'relative-minor': 'G#m'
+}
+const f_sharp_major = {
+  'key-name': 'f_sharp_major',
+  'has-sharps': true,
+  'key-signature': 6,
+  'relative-minor': 'D#m',
+  'equivalent': 'Gb'
+}
+const c_sharp_major = {
+  'key-name': 'c_sharp_major',
+  'has-sharps': true,
+  'key-signature': 7,
+  'relative-minor': 'A#m',
+  'equivalent': 'Db'
+}
+
+// KEYS WITH FLATS
+const c_flat_major = {
+  'key-name': 'c_flat_major',
+  'has-sharps': false,
+  'key-signature': 7,
+  'relative-minor': 'Abm',
+  'equivalent': 'B'
+}
+const g_flat_major = {
+  'key-name': 'g_flat_major',
+  'has-sharps': false,
+  'key-signature': 6,
+  'relative-minor': 'Ebm',
+  'equivalent': 'F#'
+}
+const d_flat_major = {
+  'key-name': 'd_flat_major',
+  'has-sharps': false,
+  'key-signature': 5,
+  'relative-minor': 'Bbm'
+}
+const a_flat_major = {
+  'key-name': 'a_flat_major',
+  'has-sharps': false,
+  'key-signature': 4,
+  'relative-minor': 'Fm'
+}
+const e_flat_major = {
+  'key-name': 'e_flat_major',
+  'has-sharps': false,
+  'key-signature': 3,
+  'relative-minor': 'Cm'
+}
+const b_flat_major = {
+  'key-name': 'b_flat_major',
+  'has-sharps': false,
+  'key-signature': 2,
+  'relative-minor': 'Gm'
+}
+const f_major = {
+  'key-name': 'f_major',
+  'has-sharps': false,
+  'key-signature': 1,
+  'relative-minor': 'Dm'
+}
+
+//#endregion
+
+//#region of roman-numerals.js
+const rn_ONE = '\u2160'
+const rn_TWO = '\u2161'
+const rn_THREE = '\u2162'
+const rn_FOUR = '\u2163'
+const rn_FIVE = '\u2164'
+const rn_SIX = '\u2165'
+const rn_SEVEN = '\u2166'
+
+const rn_one = '\u2170'
+const rn_two = '\u2171'
+const rn_three = '\u2172'
+const rn_four = '\u2173'
+const rn_five = '\u2174'
+const rn_six = '\u2175'
+const rn_seven = '\u2176'
+
+// lookup qualities: http://openmusictheory.com/triads.html
+const natural = '\u266E'
+const sharp = '\u266F'
+const flat = '\u266D'
+
+const diminished = '\u00B0'
+//#endregion
+
 // 1) Scrape through and get chord names
 const noteNameArray = ['A', 'C#', 'Dadd9']
 document.getElementById('chord-0').querySelector('.name').innerHTML = noteNameArray[0]
