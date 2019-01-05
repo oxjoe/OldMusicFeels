0368c4860fb62943a1463fdcc6b5917ba88d97e8
Joseph Pong <oxjoe2006@yahoo.com>
2019-01-05 01:07:55 -0500

    stuck on figuring out webpack (do it later)

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 import style from "./views/index.css";
+// 1) Scrape through and get chord names
+const noteNameArray = ['A', 'C#', 'Dadd9']
+document.getElementById('chord-0').querySelector('.name').innerHTML = noteNameArray[0]
+document.getElementById('chord-1').querySelector('.name').innerHTML = noteNameArray[1]
+document.getElementById('chord-2').querySelector('.name').innerHTML = noteNameArray[2]
+
+// 2) Current key would be first chord
+document.getElementById('current-key').value = a_major['key-name']
+
+// 3) Add the roman numeral
+const noteQualityArray = [rn_ONE, rn_three, rn_four]
+
+document.getElementById('chord-0').querySelector('.roman-num').innerHTML = noteQualityArray[0]
+document.getElementById('chord-1').querySelector('.roman-num').innerHTML = noteQualityArray[1]
+document.getElementById('chord-2').querySelector('.roman-num').innerHTML = noteQualityArray[2]
+
 // alert('test')
 // let express = require('express')
 // let bodyParser = require('body-parser')
